0472cb6817a3177c0e40c28286c9788605c8d75f
muhsinwarfa <muhsin.warfa@gmail.com>
2019-06-19 10:42:49 +0200

    setup configurations and database ready for model creation

---
+++ twitter/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse('Hello World! you are now seeing the idex twitter timeline')
+
+
