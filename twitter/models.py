5d2f6402da338700bca279027ca0447140a77450
muhsinwarfa <muhsin.warfa@gmail.com>
2019-06-27 18:44:54 +0200

    setup basic migrations

--- twitter/models.py
+++ twitter/models.py
@@ -1,24 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
+
 
 # Create your models here.
 class Tweet(models.Model):
-    tweet_id  = models.ForeignKey(Question, on_delete=models.CASCADE)
-    user_id = models.IntegerField()
+    tweet_id = models.IntegerField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     tweet_text = models.CharField(max_length= 150)
     time_posted = models.DateField()
-    is_retweet = models.BooleanField()
-    is_tweet = models.BooleanField()
-    is_reply = models.BooleanField()
+    is_retweet = models.IntegerField()
+    is_tweet = models.IntegerField()
+    is_reply = models.IntegerField()
     like_count = models.IntegerField()
     retweet_count = models.IntegerField()
     reply_count = models.IntegerField()
     original_tweet_id = models.IntegerField()
 
-
-
-
 class Activity(models.Model):
-    activity_id =
+    activity_id = models.IntegerField(primary_key=True)
     user_id = models.IntegerField()
     activity_name = models.CharField(max_length=50)
     user_id_2 = models.IntegerField()
