b3ea96bdd7f8cb1e826c22a89693263a4414aecc
muhsinwarfa <muhsin.warfa@gmail.com>
2019-07-26 13:59:19 +0300

    fixed dependencies incompatibility issues

--- mysite/settings.py
+++ mysite/settings.py
@@ -153,7 +153,7 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # update the db
 import dj_database_url
