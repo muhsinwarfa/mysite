dc294b7a4779fe2ed2842b67a77098d64202eb05
muhsinwarfa <muhsin.warfa@gmail.com>
2019-07-26 20:50:37 +0300

    worked on the static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,8 +133,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
@@ -143,9 +144,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
