51b3dda8ca3169af80d78d12403fb2d5cfa27369
muhsinwarfa <muhsin.warfa@gmail.com>
2019-07-27 01:45:12 +0300

    changed media directories to match collectstatic s3

--- mysite/settings.py
+++ mysite/settings.py
@@ -146,14 +146,10 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 AWS_LOCATION = 'static'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'twitter/static'),
-]
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-MEDIA_URL = 'twitter/static'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'twitter/static')
-
+STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+AWS_S3_FILE_OVERWRITE = False
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_DEFAULT_ACL = None
 
