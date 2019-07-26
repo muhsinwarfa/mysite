90de4ef1b58fd045d6a3e4673d19f98883072b5a
muhsinwarfa <muhsin.warfa@gmail.com>
2019-07-26 15:22:36 +0300

    fixed redirect issue 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -137,7 +137,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-LOGIN_REDIRECT_URL = 'list_tweets'
+LOGIN_REDIRECT_URL = '/twitter'
 
 # Deployment configs
 
