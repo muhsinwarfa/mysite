9b0621568a99bdd48daaa84679d46425de11db16
muhsinwarfa <muhsin.warfa@gmail.com>
2019-07-29 14:09:02 +0300

    changed some settings for s3 match

--- mysite/settings.py
+++ mysite/settings.py
@@ -140,8 +140,8 @@ DATABASES['default'].update(prod_db)
 #old
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.ap-northeast-2.amazonaws.com'
+AWS_STORAGE_BUCKET_NAME = 'django-twitter-files'
+AWS_S3_CUSTOM_DOMAIN = 'django-twitter-files.s3.ap-northeast-2.amazonaws.com'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
